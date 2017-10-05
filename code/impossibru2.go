6527a350ae022c4edd5ea14f154625ce74d7382c
Nicolas Ledesma <nicolas.angel.ledesma@gmail.com>
2017-10-05 03:12:39 -0300

    add slides con ejecucion de codigo

---
+++ code/impossibru2.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+type musician struct {
+	firstName  string
+	lastName   string
+	instrument string
+}
+
+// START OMIT
+func main() {
+	jimmy := musician{
+		firstName:  "Jimmy",
+		lastName:   "Page",
+		instrument: "Flute",
+	}
+	jimmy.updateInstrument("Guitar") // HL
+	fmt.Println(jimmy.firstName, "plays the", jimmy.instrument)
+}
+
+func (m *musician) updateInstrument(newInstrument string) { // HL
+	(*m).instrument = newInstrument // HL
+}
+
+// END OMIT
